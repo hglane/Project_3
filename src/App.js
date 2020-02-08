2dd4ee43249ea713c10cd81d7e8cb617b7e332ee
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-08 14:24:36 -0600

    Redo Src
    Got rid of client folder and added create-react-app based files.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+// import TaskBank from "./components/TaskBank";
+// import TaskList from "./components/TaskList";
+import './App.css';
+
+class App extends Component {
+  
+ 
+  
+  render() {
+    return (
+      <div>
+        HI!
+      </div>
+  
+     
+  
+  );
+}}
+
+
+export default App;
+ 
