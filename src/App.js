7c0a8e80f280050e5abed187ee73fb7b09842313
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-09 10:27:40 -0600

    Updated
    Basic Layout is Rendering.   Routes still need to be written.  More imports will also be needed later on.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Wrapper from "./components/Wrapper";
 // import TaskBank from "./components/TaskBank";
 // import TaskList from "./components/TaskList";
 import './App.css';
 
 class App extends Component {
-  
  
-  
   render() {
     return (
-      <div>
-        HI!
-      </div>
-  
-     
-  
-  );
-}}
-
+      <Router>
+        <div>
+          <NavBar />
+          <Wrapper>
+            <div>Temporary Div in place of Routes</div>
+            {/* <Route exact path="/" component={Calendar} />
+            <Route exact path="/tasks" component={TaskBank} />
+            <Route exact path="/tasks" component={TaskBank} />
+            <Route exact path="/tasks" component={TaskBank} /> */}
+          </Wrapper>
+        </div>
+      </Router>
+    )
+  };
+};
 
 export default App;
- 
