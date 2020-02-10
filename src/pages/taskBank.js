85e226b9a7f0421bac744dd008eca2d8e0a9e064
aces15224 <53838763+aces15224@users.noreply.github.com>
2020-02-10 15:42:41 -0600

    Added Pages
    To be filled in....

--- src/pages/taskBank.js
+++ src/pages/taskBank.js
@@ -1,1 +1,23 @@
-// fill in later
+import React from "react";
+
+
+function TaskBank() {
+    return (
+      <div>
+        <h1>Task Bank</h1>
+        <p>
+          Switch to code below once components are completed
+        </p>
+      </div>
+    );
+  }
+  
+  export default TaskBank;
+
+
+
+// class TaskBank extends React.Component {
+    
+// }
+
+// export default  TaskBank;
